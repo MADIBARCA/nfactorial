6c391fd930d04e908fe57da4a43e98871dde8cdd
e-Madi.Abzhanov <e-Madi.Abzhanov@homecredit.kz>
2024-05-10 19:51:31 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [title, setTitle] = useState([]);
+
+  const getTitle = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    return res.json()
+  }
+
+  useEffect(() => {
+    getTitle().then((res) => {
+      setTitle(res)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      {title.length > 0 && title.map((i, k) =>
+        <h1 key={k}>{i.title}</h1>
+      )
+      }
+
     </div>
   );
 }
